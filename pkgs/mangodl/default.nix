d242c6a71290d4b3d590c748b0cf7ef6d56a1434
Travis Davis <travis.davis@tech901.org>
2021-12-25 14:36:39 -0600

    add: mangodl init at v1.5

--- pkgs/mangodl/default.nix
+++ pkgs/mangodl/default.nix
@@ -1,4 +1,4 @@
-{ buildGoModule, fetchFromGitHub,...}: buildGoModule rec {
+{ buildGoModule, fetchFromGitHub, ... }: buildGoModule rec {
   pname = "mangodl";
   version = "1.5";
   src = fetchFromGitHub {
@@ -7,6 +7,6 @@
     rev = "mangodl-v${version}-linux";
     sha256 = "sha256-xFunXwu8S+/C21L1smPEosZHfJe3v54DbVhxOMI0ygw=";
   };
-  vendorSha256 ="sha256-ceARb7TlTmLpMfLESlO2OJvGDY4a1biFwJRyu2/67/o=";
+  vendorSha256 = "sha256-ceARb7TlTmLpMfLESlO2OJvGDY4a1biFwJRyu2/67/o=";
   runVend = false;
 }
