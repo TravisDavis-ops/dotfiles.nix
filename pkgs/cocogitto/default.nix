bb1b12db7cdb5df8338d172d98c53fff0597a84b
Travis Davis <travis.davis@tech901.org>
2021-12-25 14:35:16 -0600

    fix: install completions

--- pkgs/cocogitto/default.nix
+++ pkgs/cocogitto/default.nix
@@ -1,4 +1,4 @@
-{ lib, rustPlatform, fetchFromGitHub, ... }:
+{ lib, git, installShellFiles, makeWrapper, rustPlatform, fetchFromGitHub, ... }:
 rustPlatform.buildRustPackage rec {
   pname = "cocogitto";
   version = "4.0.1";
@@ -11,10 +11,32 @@ rustPlatform.buildRustPackage rec {
   cargoLock = {
     lockFile = ./Cargo.lock;
   };
-  #postPatch = ''
-  #  cp ${./Cargo.lock} Cargo.lock
-  #'';
+  nativeBuildInputs = [ installShellFiles makeWrapper git ];
+
   doCheck = false;
+  preCheck = ''
+    export HOME="$(mktemp -d)"
+    export CARGO_MANIFEST_DIR=$HOME
+    ${git}/bin/git config --global user.name John
+    ${git}/bin/git config --global user.email john@localhost
+    ${git}/bin/git config --global init.defaultBranch master
+    ${git}/bin/git config --global pull.rebase false
+    cp $source $HOME/;
+    cd $HOME/$source;
+  '';
+
+  postInstall = ''
+    installShellCompletion --cmd cog \
+      --bash <($out/bin/cog generate-completions bash) \
+      --fish <($out/bin/cog generate-completions fish) \
+      --zsh  <($out/bin/cog generate-completions zsh)
+
+    wrapProgram $out/bin/cog \
+      --prefix PATH : "${lib.makeBinPath [ git ]}"
+    wrapProgram $out/bin/coco \
+      --prefix PATH : "${lib.makeBinPath [ git ]}"
+  '';
+
   meta = with lib; {
     description =
       "Cocogitto is a set of cli tools for the conventional commit and semver specifications.";
